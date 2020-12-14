e6ebd762d437433ffa5840fdcb3f4c95c7c142ad
faniabdullah <tugasfani@gmail.com>
2020-12-14 16:00:58 +0700

    form initial

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import Form from './form.js'
+import React from 'react';
 
-const App = () => {
+class App extends React.Component {
+
+  doSomething(companyinfo) {
+    console.log(companyinfo);
+  };
+
+ render(){
   return (
     <div className="App">
       <header className="App-header">
         <h1>Company Name</h1>
       </header>
-      <Form />
+      <Form  onSubmit={this.doSomething} />
     </div>
   );
+  }
+  
 }
 
 
--- src/form.js
+++ src/form.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import axios from 'axios';
 class Form extends React.Component {
+    constructor(){
+        super();
+        this.state = { companyName: '' }
+    }
+    
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const resp = await axios.get(`https://api.github.com/users/${this.state.companyName}`);
+        this.props.onSubmit(resp.data);
+        this.setState({ companyName: '' });
+      };
+
 	render() {
   	return (
-    	<form>
-      <span className="formtext">&#x3C;Form /&#x3E;</span>
-    	  <input 
-          type="text" 
+    	<form onSubmit={this.handleSubmit} >
+          <input
+          className="form-input" 
+          type="text"
+          value={this.state.companyName} 
+          onChange={event => this.setState({ companyName: event.target.value })}
           placeholder="Enter Company Name" 
           required 
         />
